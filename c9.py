0ddd51421368ee3619779c8b71341d81b22ee097
DaveMay1983 <49055680+DaveMay1983@users.noreply.github.com>
2019-11-21 13:42:09 +0400

    Counting domain values and max domain

--- c9.py
+++ c9.py
@@ -1,100 +1,34 @@
-print("Exercise 9.2")
+# This code records the domain names and figures out which domain has the most messages in the file.
 
-fn = input("Enter a file name: ")
-fil = open(fn)
-
-d = {}
-
-for i in fil:
-   if i.startswith("From "):
-       i.rstrip()
-       l = i.split()
-       w = l[2]
-       d[w] = d.get(w, 0) + 1 
-
-print(d)
-
-
-
-print("Exercise 9.3")
-
-
-
-fn = input("Enter a file name: ")
-fil = open(fn)
+filename = input("Enter a file name: ")                    # user enters the file name
+fileopen = open(filename)                                  # opens the file in python 
  
+bar = {}                                                   # create an empty dictionary 
 
-bararan = {}
+for i in fileopen:                                         # loop through file, iterate each line (i)
+   if i.startswith("From "):                               # check if the line starts with "From ", only line contains email addresses
+       i.rstrip()                                          # right strip the line to remove whitespaces
+       l = i.split()                                       # split the line to list, which contains all the words in the line 
+       w = l[1]                                            # take the word with email address
+       f = w.find("@")                                     # find the possition of "@" char in email address string
+       m = w[f+1:]                                         # slice domain part of email address
+       bar[m] = bar.get(m, 0) + 1                          # use get function to add domain names and count them 
 
-for i in fil:
-   if i.startswith("From "):
-       i.rstrip()
-       l = i.split()
-       w = l[1]
-       bararan[w] = bararan.get(w, 0) + 1 
+# In second part of the code we find out domain with maximum messages in inbox.    
 
-print(bararan)
+mval = None                                                # set None type mval variable                                            
+mail = None                                                # set None type mail variable                                            
 
-
-
-
-
-print("Exercise 9.4")
-
-
-
-fn = input("Enter a file name: ")
-fil = open(fn)
- 
-
-bar = {}
-
-for i in fil:
-   if i.startswith("From "):
-       i.rstrip()
-       l = i.split()
-       w = l[1]
-       bar[w] = bar.get(w, 0) + 1 
-
-mval = None
-mail = None
-
-for i, j in bar.items():
-    if mval == None:
+for i, j in bar.items():                                   # loop through list of tuples of created dictionary 
+    if mval == None:                                       # set the values for mval, mail variables as first value-key pair of dictionary 
        mval = j
        mail = i
-    elif j > mval:
+    elif j > mval:                                         # find the domain with the maximaum value 
        mval = j
        mail = i  
         
-
-print(mail, mval)
-
-
-
-
-print("Exercise 9.5")
-
-
-fn = input("Enter a file name: ")
-fil = open(fn)
-
-dic = {}
-
-for i in fil:
-   if i.startswith("From "):
-       i.rstrip()
-       l = i.split()
-       w = l[1]
-       f = w.find("@")
-       m = w[f+1:]
-       dic[m] = dic.get(m, 0) + 1 
-
-print(dic)
-
-
-
-
+print("Domains with their values: ", bar)                  # print out dictionary with domain values
+print("Maximal domain and it's value: ", mail, mval)       # print out max domain and value  
 
 
 
