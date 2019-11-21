f96d7d4b53ec0baa55a7f66fa6727bf84f95e745
DaveMay1983 <49055680+DaveMay1983@users.noreply.github.com>
2019-11-21 12:00:58 +0400

    Update c8.py

--- c8.py
+++ c8.py
@@ -1,30 +1,8 @@
-print("Exercise 8.4")
-
-
-fil = open("desktop/rom.txt")
-
-l = list()
-
-for i in fil:
-   i = i.rstrip()
-   line = i.split()
-   for i in line:
-      if i not in l:
-         l.append(i)
-
-l.sort()
-  
-print(l)
-
-
-print("Exercise 8.5")
-
-
-fil = open("desktop/tex.txt")
+fileopen = open("desktop/tex.txt")
 
 count = 0
 
-for i in fil:
+for i in fileopen:
    i = i.rstrip()
    if i.startswith("From "):
       count += 1
@@ -32,25 +10,13 @@ for i in fil:
       email = line[1] 
       print(email)
 
-print(count)
+print("The number of email addresses: ", count)
 
 
 
-print("Exercise 8.6")
 
 
-l = list()
 
-while True:
-   n = input("Enter a number: ")
-   if n == "done":
-      break
-   else:
-      nb = float(n)
-      l.append(nb)
-      
-print("Maximum: ", max(l))
-print("Minimum: ", min(l))
 
 
 
