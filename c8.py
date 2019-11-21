0d743504273c6bac82cfb7ceca46a2d02caf67e6
DaveMay1983 <49055680+DaveMay1983@users.noreply.github.com>
2019-11-21 12:09:20 +0400

    Email addresses count

--- c8.py
+++ c8.py
@@ -1,16 +1,19 @@
-fileopen = open("desktop/tex.txt")
+# This code calculates the number of email addresses in mail inbox and prints out all emails.
 
-count = 0
+fileopen = open("desktop/tex.txt")                         # opens the file from directory
 
-for i in fileopen:
-   i = i.rstrip()
-   if i.startswith("From "):
-      count += 1
-      line = i.split()
-      email = line[1] 
-      print(email)
+count = 0                                                  # set the count variable, number of email addresses
 
-print("The number of email addresses: ", count)
+for i in fileopen:                                         # loop through file, iterate each line (i)
+   i = i.rstrip()                                          # right strip the line to remove whitespaces 
+   if i.startswith("From "):                               # check if the line starts with "From ", only these lines may contain inbox email address
+      count += 1                                           # count every email address in the loop
+      line = i.split()                                     # split the line and create list of words, one of them contains the email address
+      email = line[1]                                      # take email address name from the list 
+      print(email)                                         # print out email address
+
+print("The number of email addresses: ", count)            # print out the total number of email addresses
+
 
 
 
